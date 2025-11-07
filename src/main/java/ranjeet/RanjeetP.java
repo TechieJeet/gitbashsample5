9f20282da0957229eb29c4379bda04b30b6ef06e
ranjeet <=>
2025-11-07 19:48:15 +0530

    src package is created

---
+++ src/main/java/ranjeet/RanjeetP.java
@@ -0,0 +1,10 @@
+package ranjeet;
+
+public class RanjeetP {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Created by ranjeet 1st time");
+	}
+
+}
